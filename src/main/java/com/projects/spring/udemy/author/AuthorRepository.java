d18d1b8f7471ab620ba7f5713c1d3ed278d27bff
KarolXX <karol@email.com>
2021-09-17 21:41:42 +0200

    Add AuthorRepository

---
+++ src/main/java/com/projects/spring/udemy/author/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.projects.spring.udemy.author;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+}
