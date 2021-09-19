6c7242e04d954a0765a44d616da91109a5128c2a
KarolXX <karol@email.com>
2021-09-19 22:02:00 +0200

    Rename UploadImage to ImageModel

--- src/main/java/com/projects/spring/udemy/course/dto/ImageModel.java
+++ src/main/java/com/projects/spring/udemy/course/dto/ImageModel.java
@@ -2,11 +2,11 @@ package com.projects.spring.udemy.course.dto;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public class UploadImage {
+public class ImageModel {
 
     private MultipartFile file;
 
-    public UploadImage() {
+    public ImageModel() {
 
     }
 
