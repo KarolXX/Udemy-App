a7ca88d4add61a9c9a521137031d78b7514466c6
KarolXX <karol@email.com>
2021-09-27 22:13:17 +0200

    Add "ROLE_" prefix to every role of user

--- src/main/java/com/projects/spring/udemy/security/SecurityConfiguration.java
+++ src/main/java/com/projects/spring/udemy/security/SecurityConfiguration.java
@@ -2,8 +2,12 @@ package com.projects.spring.udemy.security;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -14,6 +18,16 @@ public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter 
     @Bean
     KeycloakSpringBootConfigResolver keycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Autowired
+    void configureGlobal(AuthenticationManagerBuilder auth) {
+        var authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setPrefix("ROLE_");
+        authorityMapper.setConvertToUpperCase(true);
+        KeycloakAuthenticationProvider provider = keycloakAuthenticationProvider();
+        provider.setGrantedAuthoritiesMapper(authorityMapper);
+        auth.authenticationProvider(provider);
     }
 
     @Override
