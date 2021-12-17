4876fd533f849a829a7b62e3108f102866b9d649
KarolXX <karol@email.com>
2021-12-17 23:53:10 +0100

    Make the ModelMapper work by defining a Bean

--- src/main/java/com/projects/spring/udemy/ConfigurationProperties.java
+++ src/main/java/com/projects/spring/udemy/ConfigurationProperties.java
@@ -1,5 +1,7 @@
 package com.projects.spring.udemy;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
@@ -9,6 +11,11 @@ import java.util.List;
 public class ConfigurationProperties {
     private List<String> videoExtensions;
     private String imagesPath;
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
     public List<String> getVideoExtensions() {
         return videoExtensions;
