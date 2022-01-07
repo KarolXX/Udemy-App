3a7b765d03c7be6a28405cd4e4ab478e562ff28f
KarolXX <karol@email.com>
2022-01-07 13:38:46 +0100

    Add credentials to Keycloak client

---
+++ src/main/java/com/projects/spring/udemy/KeycloakClientConfig.java
@@ -0,0 +1,33 @@
+package com.projects.spring.udemy;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakClientConfig {
+
+    @Value("${keycloak.credentials.secret}")
+    private String secret;
+
+    @Value("${keycloak.resource}")
+    private String clientId;
+
+    @Value("${keycloak.auth-server-url}")
+    private String authUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Bean
+    public Keycloak keycloak() {
+        return KeycloakBuilder.builder().grantType(OAuth2Constants.CLIENT_CREDENTIALS).serverUrl(authUrl).realm(realm)
+                .clientId(clientId)
+                .clientSecret(secret)
+                .resteasyClient(new ResteasyClientBuilder().connectionPoolSize(20).build()).build();
+    }
+}
