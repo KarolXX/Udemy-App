654de6a6033e0820e56cc15cbba617dd4dcc5340
KarolXX <karol.14.now@gmail.com>
2022-07-06 19:21:13 +0200

    Add SqlAuthorRepository interface as a bean to hide all the available methods from JpaRepository and expose only relevant ones for this app

---
+++ src/main/java/com/projects/spring/udemy/adapter/SqlAuthorRepository.java
@@ -0,0 +1,20 @@
+package com.projects.spring.udemy.adapter;
+
+import com.projects.spring.udemy.author.Author;
+import com.projects.spring.udemy.author.AuthorRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+interface SqlAuthorRepository extends AuthorRepository, JpaRepository<Author, Integer> {
+    @Query(
+            nativeQuery = true,
+            value = "SELECT a.* FROM author_course ac " +
+                    "JOIN authors a ON ac.author_id = a.author_id " +
+                    "WHERE ac.course_id = :id")
+    Optional<Author> findAuthorCourseByCourseId(@Param("id") Integer courseId);
+}
--- src/main/java/com/projects/spring/udemy/author/AuthorRepository.java
+++ src/main/java/com/projects/spring/udemy/author/AuthorRepository.java
@@ -1,22 +1,23 @@
 package com.projects.spring.udemy.author;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.keycloak.adapters.jaas.AbstractKeycloakLoginModule;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
+public interface AuthorRepository {
     Boolean existsByName(String name);
+
     Boolean existsBySalt(String name);
+
+    Optional<Author> findById(Integer id);
+
     Optional<Author> findByName(String name);
 
-    @Query(
-            nativeQuery = true,
-            value = "SELECT a.* FROM author_course ac " +
-                    "JOIN authors a ON ac.author_id = a.author_id " +
-                    "WHERE ac.course_id = :id")
     Optional<Author> findAuthorCourseByCourseId(@Param("id") Integer courseId);
+    
+    List<Author> findAll();
+
+    Author save(Author author);
 }
